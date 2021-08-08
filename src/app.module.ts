c298f40f8549f6f46bd5828c661c922d8368f466
DRIOWYA Abdel <abdodriowya@gmail.com>
2021-08-09 00:17:52 +0100

    extract item specific imports to seperate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,11 @@ import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/keys';
-import { ItemSchema } from './items/schemas/item.schema';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(config.mongoURI),
-    MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }]),
-  ],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  imports: [MongooseModule.forRoot(config.mongoURI), ItemsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
